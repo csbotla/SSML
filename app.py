59bfe80ff73ff83629da47d2da3c28d43706341c
Chandra Shekhar <cs.botla@gmail.com>
2020-02-18 20:14:50 +0530

    cleaned code app.py

--- app.py
+++ app.py
@@ -19,28 +19,14 @@ def home():
         print(url)
         article = Article(url)
 
-        # # # Do some NLP
         article.download()
         article.parse()
-        # nltk.download('punkt')
-        # article.nlp()
 
-        # #Get the authors
-        # article.authors       
-
-        # #Get the publish date
-        # article.publish_date
-
-        # #Get the top image 
-        # article.top_image
-
-        # #Get the article text
-        # print(article.text)
         fetched_article=(article.text)
         fetched_article=fetched_article.split('\n')
         
         article_title=article.title
-        # return render_template("index.html")
+        
         return render_template("index.html",fetched_article=fetched_article,article_title=article_title)
   
     
